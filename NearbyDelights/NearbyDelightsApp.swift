0b2b6447a7714cb2cccbbd52620896b59e14a9bb
GunchaSwift <gunchaswf@gmail.com>
2023-06-02 22:12:11 +0300

    Initial Commit

---
+++ NearbyDelights/NearbyDelightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NearbyDelightsApp.swift
+//  NearbyDelights
+//
+//  Created by Guntars Reiss on 02/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct NearbyDelightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
